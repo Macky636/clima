f444191acc8e9138304bef539e61e452f6de597d
Axel Tdo <axelromtad@gmail.com>
2025-04-17 12:37:12 -0600

    Initial Commit

---
+++ clima/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clima
+//
+//  Created by axel tadeo on 17/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ clima/climaApp.swift
@@ -0,0 +1,17 @@
+//
+//  climaApp.swift
+//  clima
+//
+//  Created by axel tadeo on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct climaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
